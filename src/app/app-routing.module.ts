c226a913ba0f0afff62c7f85bc1734a3a6e9685b
AlessandroCristiano <alessandrocristiano66@gmail.com>
2022-12-16 12:18:54 +0100

    Aggiunte le guardie quando non sei loggato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   { 
@@ -8,15 +9,18 @@ const routes: Routes = [
   },
   { 
     path: 'welcome', 
-    loadChildren:()=> import('./features/welcome/welcome.module').then(m => m.WelcomeModule)
+    loadChildren:()=> import('./features/welcome/welcome.module').then(m => m.WelcomeModule),
+    canActivate: [AuthGuard]
   },
   { 
     path: 'regista', 
-    loadChildren:()=> import('./features/regista/regista.module').then(m => m.RegistaModule)
+    loadChildren:()=> import('./features/regista/regista.module').then(m => m.RegistaModule),
+    canActivate: [AuthGuard]
   },
   { 
     path: 'film', 
-    loadChildren:()=> import('./features/film/film.module').then(m => m.FilmModule)
+    loadChildren:()=> import('./features/film/film.module').then(m => m.FilmModule),
+    canActivate: [AuthGuard]
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
